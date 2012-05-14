051afa078d99596f20d19b9a12634c4d37a3a3ae
elmotec <jlecomte1972@yahoo.com>
2012-05-13 20:37:19 -0400

    replaced shell script with dotfiles python script

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-typeset module=`basename $0 .sh`
-typeset srcdir=`dirname $0`
-srcdir=`(cd $srcdir && pwd)`
-typeset home=${HOME:?"missing environment variable"}
-
-# Creates relative symlinks to dotfiles contents.
-ls -a $srcdir | egrep -v "(\.git|README.rst|\..*\.swp|.*\.log|\.|\.\.|$module\..*)$" | sed "s|.*|ln -s $srcdir/& $HOME/.|g" | sh
