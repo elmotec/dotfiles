8ee18dd3eaf5f617445f408c11f879a84863cf3c
elmotec <jlecomte1972@yahoo.com>
2012-05-16 08:44:05 -0400

    started work on reporting

--- manage.py
+++ manage.py
@@ -3,23 +3,23 @@
 """Manages dot file relationship between home and dotfiles directories."""
 
 # Copyright (c) 2012 Jerome Lecomte
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy 
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights 
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is 
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in 
+#
+# The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
 
@@ -29,11 +29,13 @@ __license__ = 'MIT'
 
 
 import sys
+import os.path
 import logging
 import argparse
 import difflib
 import types
 import glob
+import filecmp
 
 
 logger = logging.getLogger(__name__)
@@ -46,9 +48,95 @@ class DotfileError(RuntimeError):
     pass
 
 
-class DotfileManager(object):
-    pass
+class Dotfile(object):
+    """Abstraction for a (possible) link between repository and home dir."""
 
+    synced='synced'
+    external='external'
+    missing='missing'
+    conflict='conflict'
+    same='same'
+
+    def __init__(self, dotfile, status=None):
+        self.dotfile = dotfile
+        self.status = status
+
+    def __repr__(self):
+        return "<{}: {}>".format(self.__class__.__name__, vars(self))
+
+    def __eq__(self, rhs):
+        return self.dotfile == rhs.dotfile and \
+                self.status == rhs.status
+
+class DotfileManager(object):
+    """Manages dotfiles."""
+
+    valid_commands = ['help', 'report']
+
+    def __init__(self, home_dir=None, dotfiles_dir=None):
+        """Sets variable that will be needed by the manager.
+      
+        Post conditions:
+        self.home_dir exists and is a directory.
+        self.dotfiles_dir exists and is a directory.
+        """
+        self.home_dir = os.path.expanduser("~")
+        if home_dir:
+            self.home_dir = home_dir 
+        self.dotfiles_dir = os.path.join(self.home_dir, 'dotfiles')
+        if dotfiles_dir:
+            self.dotfiles_dir = dotfiles_dir
+        assert(self.invariants())
+
+    def invariants(self):
+        """Self check the class. Called with assert(invariants())."""
+        assert(os.path.exists(self.home_dir))
+        assert(os.path.isdir(self.home_dir))
+        #assert(os.path.exists(self.dotfiles_dir) or 
+                #os.path.exists(os.path.join(self.home_dir, self.dotfiles_dir)))
+        #assert(os.path.isdir(self.dotfiles_dir) or
+                #os.path.exists(os.path.join(self.home_dir, self.dotfiles_dir)))
+        return True
+
+    def get_dotfiles_abspath(self):
+        """Return the absolute path to the dotfile directory"""
+        if os.path.abspath(self.dotfiles_dir) == self.dotfiles_dir:
+            return self.dotfiles_dir
+        return os.path.join(self.home_dir, self.dotfiles_dir)
+
+    def get_dotfiles(self):
+        """Gets a generator that loops through the dotfiles."""
+        for root, dirnames, filenames in os.walk(self.get_dotfiles_abspath()):
+            for filename in filenames:
+                home_filename = os.path.join(self.home_dir, filename)
+                dotfile_name = os.path.join(
+                        self.get_dotfiles_abspath(), filename)
+                if os.path.islink(home_filename):
+                    target = os.readlink(home_filename)
+                    if target == dotfile_name:
+                        status = Dotfile.synced
+                    else:
+                        status = Dotfile.external
+                elif not os.path.exists(home_filename):
+                    home_filename = None
+                    status = Dotfile.missing
+                elif filecmp.cmp(dotfile_name, home_filename):
+                    status = Dotfile.same
+                else:
+                    status = Dotfile.conflict
+                yield Dotfile(filename, status=status) 
+    
+    def help(self):
+        """This help"""
+        print("valid commands are:")
+        for command in self.valid_commands:
+            print("  {:<10}: {}".format(command, 
+                getattr(self, command).__doc__))
+
+    def report(self):
+        """Displays the status of the dotfiles"""
+        pass  # FIXME 
+        
 
 def get_verbosity(verbose_count):
     """Helper to convert a count of verbosity level to a logging level."""
@@ -90,16 +178,18 @@ def command_line(argv):
             help="increases log verbosity (can be specified multiple times)")
     parser.add_argument('command', metavar="command",
             help="command to exexute")
-    parser.add_argument('dotfiles', metavar="dotfile", nargs='+',
+    parser.add_argument('dotfiles', metavar="dotfile", nargs='*',
             help="dot files to process. See commands.")
     arguments = parser.parse_args(argv[1:])
     setup_logger(int(arguments.verbose_count))
     manager = DotfileManager()
     dotfiles = arguments.dotfiles
     if sys.platform == 'win32':
-        files = expand_wildcards(files)
-    for dotfile in dotfiles:
-        manager.process(dotfile)
+        dotfiles = expand_wildcards(dotfiles)
+    if arguments.command == 'help':
+        manager.help()
+    if arguments.command == 'report':
+        manager.report()
     return 1
 
 
@@ -112,3 +202,5 @@ if __name__ == "__main__":
     finally:
         logging.shutdown()
     sys.exit(os_status)
+
+
--- test_manage.py
+++ test_manage.py
@@ -1,34 +1,121 @@
 #!/usr/bin/env python
 
+"""Functional tests for py."""
+
 # Copyright (c) 2012 Jerome Lecomte
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy 
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights 
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is 
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in 
+#
+# The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
 import unittest
+import tempfile
+import os.path
+
+from manage import Dotfile, DotfileManager
 
 class DotfileManagerTest(unittest.TestCase):
     def setUp(self):
-        pass
+        """Common initialization for tests.
+        
+        Creates a fake home directory to work with and have a dot file
+        manager object set up with it.
+        """
+        home_dir = tempfile.mkdtemp()
+        dotfiles_dir = os.path.join(home_dir, 'dotfiles')
+        os.mkdir(dotfiles_dir) 
+        self.dfm = DotfileManager(home_dir=home_dir,
+                dotfiles_dir=os.path.basename(dotfiles_dir))
+        self.file_names = []
+        self.directories = [dotfiles_dir, home_dir]  # order matters!
+        self.file_name = '.dotfilerc'
+        self.create_file(self.dfm.dotfiles_dir, self.file_name, "[dotfile]") 
 
     def tearDown(self):
-        pass
+        """Cleans up test home and dotfiles directories."""
+        for filename in self.file_names:
+            os.remove(filename)
+        for directory in self.directories:
+            os.rmdir(directory)
+
+    def assertExists(self, path):
+        """Helper to assert a path exists."""
+        self.assertTrue(os.path.exists(path), "{} does not exist".format(path))
+
+    def assertDirExists(self, directory):
+        """Helper to assert a path exists and is a directory."""
+        self.assertExists(directory)
+        self.assertTrue(os.path.isdir(directory),
+                "{} is not a directory".format(directory))
+
+    def create_file(self, dirname, filename, content):
+        if not os.path.exists(dirname):
+            dirname = os.path.join(self.dfm.home_dir, dirname)
+        filepath = os.path.join(dirname, filename)
+        with open(filepath, 'w') as fh:
+            fh.write(content)
+        self.file_names.append(filepath)
+
+    def create_symlink(self, dirname, filename, target):
+        filepath = os.path.join(dirname, filename)
+        os.symlink(target, filepath)
+        self.file_names.append(filepath)
+
+    def test_home_dir_exists(self):
+        """Checks the home directory is set up and is a directory."""
+        self.assertDirExists(self.dfm.home_dir)
+
+    def test_dotfile_dir_exists(self):
+        """Checks the dotfile directory is set up and is a directory."""
+        self.assertDirExists(self.dfm.get_dotfiles_abspath())
+
+    def test_report_missing(self):
+        dotfiles = list(self.dfm.get_dotfiles())
+        expected_dotfile = Dotfile(self.file_name, status='missing')
+        self.assertSequenceEqual(dotfiles, [expected_dotfile])
+
+    def test_report_same(self):
+        self.create_file(self.dfm.home_dir, self.file_name, "[dotfile]")
+        dotfiles = list(self.dfm.get_dotfiles())
+        expected_dotfile = Dotfile(self.file_name, status=Dotfile.same)
+        self.assertSequenceEqual(dotfiles, [expected_dotfile])
+
+    def test_report_conflict(self):
+        self.create_file(self.dfm.home_dir, self.file_name, "[dotfiles]")
+        dotfiles = list(self.dfm.get_dotfiles())
+        expected_dotfile = Dotfile(self.file_name, status=Dotfile.conflict) 
+        self.assertSequenceEqual(dotfiles, [expected_dotfile])
+
+    def test_report_external(self):
+        dotfiles_dir = os.path.basename(self.dfm.home_dir)
+        target = os.path.join(dotfiles_dir, self.file_name)
+        self.create_symlink(self.dfm.home_dir, self.file_name, 'bogus')
+        dotfiles = list(self.dfm.get_dotfiles())
+        expected_dotfile = Dotfile(self.file_name, status=Dotfile.external) 
+        self.assertSequenceEqual(dotfiles, [expected_dotfile])
+
+    def test_report_synced(self):
+        target = os.path.join(self.dfm.dotfiles_dir, self.file_name)
+        self.create_symlink(self.dfm.home_dir, self.file_name, target)
+        dotfiles = list(self.dfm.get_dotfiles())
+        expected_dotfile = Dotfile(self.file_name, status=Dotfile.synced) 
+
 
 if __name__ == '__main__':
     unittest.main()
+
 
