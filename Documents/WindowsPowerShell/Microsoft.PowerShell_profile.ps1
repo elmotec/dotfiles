f4fdb7c529c63dd34f585ee734da128426811fd0
elmotec <elmotec@gmx.com>
2020-04-11 10:24:33 -0400

    Added fzf ctrl+t in powershell

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,12 @@
 Set-Alias vi nvim
 Set-Alias gvi nvim-qt
+Set-Alias which Get-Command
+
+Add-PathVariable 'C:\Program Files\Git\bin\'
+Add-PathVariable 'C:\Users\jlecomte\AppData\Local\Programs\Python\Python38'
+Add-PathVariable 'C:\Program Files\WinMerge\'
+
+# Turn on fzf at the command line.
+Import-Module PSFzf -ArgumentList 'Ctrl+t', 'Alt+r' -Force
+
+
