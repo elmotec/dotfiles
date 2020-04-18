dafe58ef4e8bd8525bb72ed06ec3a3fad966293e
elmotec <elmotec@gmx.com>
2020-04-18 12:25:01 -0400

    Make nvim the default $Env:EDITOR
    Also fixed setting of PATH environment variable to be compatible with
    older version of Powershell.

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,12 +2,14 @@ Set-Alias vi nvim
 Set-Alias gvi nvim-qt
 Set-Alias which Get-Command
 
-Add-PathVariable "$home\scripts"
-Add-PathVariable 'C:\Program Files\Git\bin\'
-Add-PathVariable 'C:\Users\jlecomte\AppData\Local\Programs\Python\Python38'
-Add-PathVariable 'C:\Program Files\WinMerge\'
+$Env:Path += ";$home\scripts"
+$Env:Path += ';C:\Program Files\Git\bin\'
+$Env:Path += ';C:\Users\jlecomte\AppData\Local\Programs\Python\Python38'
+$Env:Path += ';C:\Program Files\WinMerge\'
 
 # Turn on fzf at the command line.
 Import-Module PSFzf -ArgumentList 'Ctrl+t', 'Alt+r' -Force
 
+# Set the virual editor for subversion, git, ...
+$Env:EDITOR="nvim.exe"
 
