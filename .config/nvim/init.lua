f5345a0b495e2a7eff61828c055a96075c618475
elmotec <1107551+elmotec@users.noreply.github.com>
2023-06-19 10:47:58 -0400

    feat(vim): fix sumneko_lua deprecation message
    Also add tips on startup, asyncrun.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -221,30 +221,28 @@ require 'lspconfig'.clangd.setup {
 }
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#cmake
 require 'lspconfig'.cmake.setup {}
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
-require 'lspconfig'.sumneko_lua.setup {
-    on_attach = on_attach,
-    flags = lsp_flags,
-    settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = { 'vim' },
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-                enable = false,
-            },
-        },
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+require'lspconfig'.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
     },
+  },
 }
 
 require 'lspconfig'.dockerls.setup {
